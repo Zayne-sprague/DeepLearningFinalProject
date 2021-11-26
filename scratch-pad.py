13ac3f64369c99fa39616505efefb2bd08af7fb3
Zayne-sprague <zayne_sprague@yahoo.com>
2021-11-25 20:05:37 -0600

    More activations

--- scratch-pad.py
+++ scratch-pad.py
@@ -26,6 +26,26 @@ def accelerator(patch: tensor, influence: float = 0.1) -> tensor:
     patch[patch < 0] = 0
     return patch
 
+def excitator2(patch: tensor, influence: float = 0.1) -> tensor:
+    impact = patch[patch > 0].sum() * influence
+
+    num_non_positives = len(patch <= 0)
+    if num_non_positives == 0:
+        return patch
+
+    patch[patch > 0] *= 1 - influence
+    patch[patch <= 0] += impact / num_non_positives
+
+    patch[patch < 0] = 0
+    return patch
+
+
+def tmp1(patch: tensor) -> tensor:
+    # Max relu in patch
+    patch[patch < 0] = 0
+    patch[patch < torch.max(patch)] = 0
+    return patch
+
 
 def inhibitor(patch: tensor, influence: float = 0.1) -> tensor:
     impact = patch[patch < 0].sum() * influence
@@ -34,14 +54,30 @@ def inhibitor(patch: tensor, influence: float = 0.1) -> tensor:
     patch[patch < 0] = 0
     return patch
 
+def inhibitor2(patch: tensor, influence: float = 0.1) -> tensor:
+    impact = patch[patch < 0].sum() * influence
+
+    num_of_positives = len(patch >= 0)
+    if num_of_positives == 0:
+        patch[patch < 0] = 0
+        return patch
+
+    patch[patch < 0] *= 1 - influence
+    patch[patch > 0] += impact / num_of_positives
+    patch[patch < 0] = 0
+    return patch
+
+def sftmx_tmp(patch: tensor, threshold: float = 0.5) -> tensor:
+    weights = torch.softmax(patch, dim=0)
+    patch[weights < threshold] = 0
+    return patch
+
 
 def patcher(img: tensor, k: int = 2) -> Union[tensor, Callable]:
     batch_size = img.shape[0]
     patches: tensor = img.unfold(1, 1, 1).unfold(2, k, k).unfold(3, k, k)
     unfold_shape = patches.shape
     patches = patches.contiguous().view(-1, 1, k, k)
-
-
 
     return patches, partial(stitcher, batch_size=batch_size, unfold_shape=unfold_shape)
 
@@ -124,15 +160,40 @@ if __name__ == "__main__":
         print(passive_nelu_patch)
         print("\n\n")
 
-
-        accelerator_patch = accelerator(patch.clone(), influence=0.33)
-
-        print("ACCELERATOR:")
-        print(accelerator_patch)
-        print("\n\n")
-
         inhibitor_patch = inhibitor(patch.clone(), influence=0.33)
 
         print("INHIBITOR:")
         print(inhibitor_patch)
+        print("\n\n")
+
+        inhibitorv2_patch = inhibitor2(patch.clone(), influence=0.33)
+
+        print("INHIBITOR v2:")
+        print(inhibitorv2_patch)
+        print("\n\n")
+
+
+        tmp1_patch = tmp1(patch.clone())
+
+        print("TMP1:")
+        print(tmp1_patch)
+        print("\n\n")
+
+        softmax_tmp_patch = sftmx_tmp(patch.clone(), threshold=0.33)
+
+        print("SOFTMAX TMP:")
+        print(softmax_tmp_patch)
+        print("\n\n")
+
+
+        accelerator_patch = accelerator(patch.clone(), influence=0.2)
+
+        print("EXCITATOR v1:")
+        print(accelerator_patch)
+        print("\n\n")
+
+        excitator2_patch = excitator2(patch.clone(), influence=0.2)
+
+        print("Excitator v2:")
+        print(excitator2_patch)
         print("\n\n")
